b96c7063941e89154347afcca45f6195380ce8ae
rlaquea <rlaquea14@gmail.com>
2021-08-30 13:02:55 -0400

    added nav

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -1,14 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const Header = () => {
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+} from 'reactstrap';
+
+import { Link } from 'react-router-dom';
+
+const Header = (props) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggle = () => setIsOpen(!isOpen);
+
   return (
-    <header className="bg-primary text-light mb-4 py-3 flex-row align-center">
-      <div className="container flex-row justify-space-between-lg justify-center align-center">
-        <h1 className="m-0">Sound Therapy App</h1>
-        <p className="m-0">Interactive app for sound</p>
-      </div>
-    </header>
+    <div>
+      <Navbar color="light" light expand="md">
+        <NavbarBrand href="/">reactstrap</NavbarBrand>
+        <NavbarToggler onClick={toggle} />
+        <Collapse isOpen={isOpen} navbar>
+          <Nav className="mr-auto" navbar>
+            <NavItem>
+              <Link to="/">Home</Link>
+              <Link to="/">Explore</Link>
+              <Link to="/">Daily Exercises</Link>
+              <Link to="/">Create</Link>
+              <Link to="/">Favorites</Link>
+            </NavItem>
+            <NavItem>
+              <Link href="https://github.com/reactstrap/reactstrap">GitHub</Link>
+            </NavItem>
+          </Nav>
+        </Collapse>
+      </Navbar>
+    </div>
   );
-};
-
+}
 export default Header;
