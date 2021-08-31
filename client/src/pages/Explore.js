fcf044543c4f4e4684156497ad33a34e2875a146
rlaquea <rlaquea14@gmail.com>
2021-08-31 14:34:37 -0400

    added random num for random sound functionality

--- client/src/pages/Explore.js
+++ client/src/pages/Explore.js
@@ -1,18 +1,13 @@
 import React from 'react';
-import generateSound from '../utils/generateRandSound';
+import GenerateSound  from '../utils/generateRandSound';
 
-function Explore() {
-  //create hook & arrow function
-  <script>
-    document.getElementById("randomSound").innerHTML =
-    Math.floor(Math.random() * 100);
-    console.log(randomSound);
-  </script>
+const Explore = () => {
+
   return (
     <div>
       <p>There's a world of sound out there just waiting to be discovered.</p>
       <p>Click the button below to explore!</p>
-      <button id="randomSound">{generateSound}Generate</button>
+    <GenerateSound/>
     </div>
 
   )
