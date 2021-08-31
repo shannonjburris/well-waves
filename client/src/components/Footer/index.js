f72e22232201ab362f7245351988a740f59f11ab
Alex Abbamondi <alex.abbamondi@gmail.com>
2021-08-31 16:24:16 -0400

    added logout functionality

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -1,12 +1,21 @@
-import React from 'react';
-import './Footer.css';
+import React from "react";
+
 const Footer = () => {
   return (
     <footer className="w-100 mt-auto bg-secondary p-4">
-     <div class="container">
-                <div class="links">
-                <a href="https://github.com/RLAQUEA/final-project" target="_blank" rel="noreferrer"><i class="fab fa-github"></i></a>
-                </div>
-            </div>
-            </footer>
-  )};
+      <div class="container">
+        <div class="links">
+          <a
+            href="https://github.com/RLAQUEA/final-project"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <i class="fab fa-github"></i>
+          </a>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
